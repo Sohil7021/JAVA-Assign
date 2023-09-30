ffd1792468d4f984bcab09f94a443c243bdb981f
SohilSheikh <SheikhSohil7021@gmail.com>
2023-09-30 10:02:40 +0530

    solve array question

---
+++ JAVA-Assignment/arrays/src/Reshape_the_Matrix.java
@@ -0,0 +1,11 @@
+public class Reshape_the_Matrix {
+    public static void main(String[] args) {
+        int[][] mat = {
+                {1,2},
+                {3,4}
+        };
+    }
+    static  int[][] matrixReshape(int[][] mat,int r,int c) {
+
+    }
+}
