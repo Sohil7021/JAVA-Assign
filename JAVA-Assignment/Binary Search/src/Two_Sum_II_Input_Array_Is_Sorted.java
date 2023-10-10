c51acf66619c868e992fa8e4174e1bb16c04c25c
SohilSheikh <SheikhSohil7021@gmail.com>
2023-10-10 09:17:34 +0530

    binary Search assignment

--- JAVA-Assignment/Binary Search/src/Two_Sum_II_Input_Array_Is_Sorted.java
+++ JAVA-Assignment/Binary Search/src/Two_Sum_II_Input_Array_Is_Sorted.java
@@ -2,29 +2,66 @@ import java.util.Arrays;
 
 public class Two_Sum_II_Input_Array_Is_Sorted {
     public static void main(String[] args) {
-            int[] numbers = {12,13,23,28,43,44,59,60,61,68,70,86,88,92,124,125,136,168,173,173,180,199,212,221,227,230,277,282,306,314,316,321,325,328,336,337,363,365,368,370,370,371,375,384,387,394,400,404,414,422,422,427,430,435,457,493,506,527,531,538,541,546,568,583,585,587,650,652,677,691,730,737,740,751,755,764,778,783,785,789,794,803,809,815,847,858,863,863,874,887,896,916,920,926,927,930,933,957,981,997,
-        542};
-            int[] result = twoSum(numbers,9);
+        int[] numbers = {2,7,11,15};
+        int[] result = twoSum2(numbers, 9);
         System.out.println(Arrays.toString(result));
     }
-    static int[] twoSum(int[] numbers,int target) {
-        for (int i = 0; i < numbers.length ; i++) {
+
+    static int[] twoSum(int[] numbers, int target) {
+        for (int i = 0; i < numbers.length; i++) {
             int complement = target - numbers[i];
-            int start = i+1;
+            int start = i + 1;
             int end = numbers.length - 1;
 
             while (start <= end) {
                 int mid = start + (end - start) / 2;
                 if (numbers[mid] == complement) {
-                    return new int[] {i+1,mid+1};
-                }else if (numbers[mid] < complement) {
+                    return new int[]{i + 1, mid + 1};
+                } else if (numbers[mid] < complement) {
                     start = mid + 1;
-                }else {
+                } else {
                     end = mid - 1;
                 }
             }
         }
-        return new int[] {-1,-1};
+        return new int[]{-1, -1};
+    }
+
+    static int[] twoSum2(int[] numbers, int target) {
+        int start = 0;
+        int end = numbers.length - 1;
+        while (numbers[start] + numbers[end] != target) {
+            if (numbers[start] + numbers[end] > target) {
+                if (numbers[end - 1] > target - numbers[start]) {
+                    end = binarySearch(numbers, target - numbers[start], start + 1, end - 1);
+                } else {
+                    end--;
+                }
+
+            } else if (numbers[start+1] < target - numbers[end]) {
+                start = binarySearch(numbers,target-numbers[end],start+1,end-1);
+            }else {
+                start++;
+            }
+        }
+        return new int[]{start+1,end+1};
+    }
+
+
+
+
+    static int binarySearch(int[] numbers,int target,int start, int end) {
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if(numbers[mid] == target) {
+                return mid;
+            } else if (numbers[mid] > target) {
+                return binarySearch(numbers,target,0,end - 1);
+            }else {
+                return binarySearch(numbers,target,mid + 1,end);
+            }
+        }
+        return -1;
     }
 
 }
