1e430428da3fb5a3d9fb6a385abe0ade6a6604a4
SohilSheikh <SheikhSohil7021@gmail.com>
2023-11-12 10:03:17 +0530

    strings questions

--- String/src/Main/Defanging_an_IP_Address.java
+++ String/src/Main/Defanging_an_IP_Address.java
@@ -24,4 +24,20 @@ public class Defanging_an_IP_Address {
         return sb.toString();
 
     }
+
+    public static class Shuffle_String {
+        public static void main(String[] args) {
+          String s = "codeleet";
+          int[] indices = {4,5,6,7,0,2,1,3};
+          String result = restoreString(s,indices);
+            System.out.println(result);
+        }
+        static String restoreString(String s, int[] indices) {
+           char[] temp = new char[indices.length];
+            for (int i = 0; i <s.length() ; i++) {
+                temp[indices[i]] = s.charAt(i);
+            }
+            return new String(temp);
+        }
+    }
 }
